d2c2c0d7bf6a4277b2c20884f33102ef8b525e43
LXaburtoM <lxaburto@uam.edu.ni>
2026-08-31 16:23:10 -0600

    "feat: finalizacion  del class de CafeteriaController"

--- Caferteria/src/main/java/module-info.java
+++ Caferteria/src/main/java/module-info.java
@@ -2,7 +2,15 @@ module ni.edu.uam.caferteria {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    // Permite a JavaFX arrancar la aplicación principal
     opens ni.edu.uam.caferteria to javafx.fxml;
     exports ni.edu.uam.caferteria;
+
+    // Permite al FXML inyectar los botones y cajas de texto en tu controlador
+    opens ni.edu.uam.caferteria.controller to javafx.fxml;
+    exports ni.edu.uam.caferteria.controller;
+
+    // Permite a la TableView (javafx.base) leer los datos de tu clase LoteCafe
+    opens ni.edu.uam.caferteria.modelo to javafx.base;
+    exports ni.edu.uam.caferteria.modelo;
 }
