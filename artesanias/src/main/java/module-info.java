a312251c3bc1205017f59006891bb9d05ec62d89
LXaburtoM <lxaburto@uam.edu.ni>
2026-09-01 15:00:46 -0600

    feat: Actualizar la vista y el tamaño de la ventana en HelloApplication

--- artesanias/src/main/java/module-info.java
+++ artesanias/src/main/java/module-info.java
@@ -2,7 +2,15 @@ module ni.edu.uam.artesanias {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    // Permite iniciar la aplicación principal
     opens ni.edu.uam.artesanias to javafx.fxml;
     exports ni.edu.uam.artesanias;
+
+    // Da permisos para que la vista use el controlador
+    opens ni.edu.uam.artesanias.controller to javafx.fxml;
+    exports ni.edu.uam.artesanias.controller;
+
+    // Da permisos para que la tabla pueda leer la clase Artesania
+    opens ni.edu.uam.artesanias.modelo to javafx.base;
+    exports ni.edu.uam.artesanias.modelo;
 }
--- artesanias/src/main/java/ni/edu/uam/artesanias/HelloApplication.java
+++ artesanias/src/main/java/ni/edu/uam/artesanias/HelloApplication.java
@@ -8,12 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+        @Override
+        public void start(Stage stage) throws IOException {
+            // Carga la vista que armamos
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("artesania-view.fxml"));
+
+            // Ajustamos la ventana al tamaño que definimos en el FXML (750x500)
+            Scene scene = new Scene(fxmlLoader.load(), 750, 500);
+
+            stage.setTitle("Tienda de Artesanías - Catálogo");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch();
+        }
 }
